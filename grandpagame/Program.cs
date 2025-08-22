feb4896e65bd3dd847bd72a2503fed83f1c5de29
rogdesune <rogerioalencarfinal21@gmail.com>
2025-08-22 12:30:32 -0300

    alterado o método de empate de for para foreach

--- grandpagame/Program.cs
+++ grandpagame/Program.cs
@@ -131,16 +131,13 @@ class Program
     }
     static bool VerificarEmpate(char[,] tabuleiro)
     {
-        for (int i = 0; i < 3; i++)
+        foreach (char c in tabuleiro)
         {
-            for (int j = 0; j < 3; j++)
+            if (c == ' ')
             {
-                if (tabuleiro[i, j] == ' ')
-                {
-                    return false;
-                }
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
