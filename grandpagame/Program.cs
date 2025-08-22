eeca039aafe4e59b9aa755a70eac0cb1222bf499
rogdesune <rogerioalencarfinal21@gmail.com>
2025-08-22 11:36:56 -0300

    Verificação de inputs no tabuleiro para efetuar a jogada, e verificação de coordenadas válidas.

--- grandpagame/Program.cs
+++ grandpagame/Program.cs
@@ -11,13 +11,34 @@ class Program
         while (jogoAtivo)
         {
             Console.WriteLine($"\nVez do jogador {jogadorAtual}");
-
+            int linha, coluna;
             while (true)
             {
                 MostrarTabuleiro(tabuleiro);
-                break;
+                
+                Console.WriteLine("\nDigite o número da linha: (0-2)");
+                int.TryParse(Console.ReadLine(), out linha);
+                Console.WriteLine("\nDigite o número da linha: (0-2)");
+                int.TryParse(Console.ReadLine(), out coluna);
+
+                if (linha >= 0 && linha < 3 && coluna >= 0 && coluna < 3)
+                {
+                    if (tabuleiro[linha, coluna] == ' ')
+                    {
+                        // posição válida.
+                        Console.WriteLine($"Posição escolhida [{linha}, {coluna}] é válida.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Coordenas já ocupada. Tente novamente.");
+                    }
+                } else
+                {
+                    Console.WriteLine("Coordenadas inválidas. Tente novamente.");
+                }
             }
-            jogoAtivo = false;
+            tabuleiro[linha, coluna] = jogadorAtual;
         }
     }
     static void IniciarTabuleiro(char[,] tabuleiro)
