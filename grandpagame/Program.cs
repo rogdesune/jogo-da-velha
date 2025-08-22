a379d2c2e6f69ad9fc3620996aa0f007f5db2197
rogdesune <rogerioalencarfinal21@gmail.com>
2025-08-22 12:02:42 -0300

    método para verificar vitória do jogador atual

--- grandpagame/Program.cs
+++ grandpagame/Program.cs
@@ -67,4 +67,48 @@ class Program
             if (i < 2) Console.WriteLine("  ---+---+---");
         }
     }
+    static bool VerificarVitoria(char[,] tabuleiro, char jogadorAtivo)
+    {
+        // linhas
+        for (int i = 0; i < 3; i++)
+        {
+            if (tabuleiro[i, 0] == jogadorAtivo && tabuleiro[i, 1] == jogadorAtivo && tabuleiro[i, 2] == jogadorAtivo)
+            {
+                return true;
+            }
+        }
+        // colunas
+        for (int j = 0; j < 3; j++)
+        {
+            if (tabuleiro[0, j] == jogadorAtivo && tabuleiro[1, j] == jogadorAtivo && tabuleiro[j, 2] == jogadorAtivo)
+            {
+                return true;
+            }
+        }
+        // diagonal \
+        /*
+           0 1 2
+         0 x| |  
+         1  |x|
+         2  | |x
+
+         */
+        if (tabuleiro[0, 0] == jogadorAtivo && tabuleiro[1, 1] == jogadorAtivo && tabuleiro[2, 2] == jogadorAtivo)
+        {
+            return true;
+        }
+        // diagonal /
+        /*
+           0 1 2
+         0  | |O  
+         1  |O|
+         2 O| | 
+
+         */
+        else if (tabuleiro[2, 0] == jogadorAtivo && tabuleiro[1, 1] == jogadorAtivo && tabuleiro[0, 2] == jogadorAtivo)
+        {
+            return true;
+        }
+            return false;
+    }
 }
